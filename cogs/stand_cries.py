651ba0ba59c4b0911a98635868e16f6dd3b22172
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-24 23:09:15 +0400

    im so stupid cri

--- cogs/stand_cries.py
+++ cogs/stand_cries.py
@@ -7,21 +7,21 @@ class Cries(commands.Cog):
 
 
 
-@commands.command(name = "ora")
-async def ora(ctx):
-    await ctx.send("ORA ORA ORA ORA ORA!")
+    @commands.command(name = "ora")
+    async def ora(ctx):
+        await ctx.send("ORA ORA ORA ORA ORA!")
 
-@commands.command(name = "muda")
-async def muda(ctx):
-    await ctx.send("MUDA MUDA MUDA MUDA MUDA!")
+    @commands.command(name = "muda")
+    async def muda(ctx):
+        await ctx.send("MUDA MUDA MUDA MUDA MUDA!")
 
-@commands.command(name = "dora")
-async def dora(ctx):
-    await ctx.send("DORA DORA DORA DORA DORA!")
+    @commands.command(name = "dora")
+    async def dora(ctx):
+        await ctx.send("DORA DORA DORA DORA DORA!")
     
-@commands.command(name = "arri")
-async def arri(ctx):
-    await ctx.send("ARRI ARRI ARRI ARRI ARRIVEDERCI!")
+    @commands.command(name = "arri")
+    async def arri(ctx):
+        await ctx.send("ARRI ARRI ARRI ARRI ARRIVEDERCI!")
 
 def setup(client):
     client.add_cog(Cries(client))
