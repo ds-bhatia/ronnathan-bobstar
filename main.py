cd5b39ae241ef0b95ec68bc5ae2b4dc60869de4c
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-24 20:13:10 +0400

    added ora ora!

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ token = os.environ("TOKEN")
 
 client = commands.Bot(command_prefix = "j!")
 
+@client.command(name = "ora")
+async def ora(self, ctx):
+    await ctx.send("ORA ORA ORA ORA ORA!")
+
+
 client.run(token)
