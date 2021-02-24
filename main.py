6ac186f158c7b6aa2b421641777f9973cd18c2ea
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-24 22:48:20 +0400

    sorted stand cries into cog

--- main.py
+++ main.py
@@ -6,23 +6,6 @@ token = os.environ["TOKEN"]
 
 client = commands.Bot(command_prefix = "j!")
 
-
-@client.command(name = "ora")
-async def ora(ctx):
-    await ctx.send("ORA ORA ORA ORA ORA!")
-
-@client.command(name = "muda")
-async def muda(ctx):
-    await ctx.send("MUDA MUDA MUDA MUDA MUDA!")
-
-@client.command(name = "dora")
-async def dora(ctx):
-    await ctx.send("DORA DORA DORA DORA DORA!")
-    
-@client.command(name = "arri")
-async def arri(ctx):
-    await ctx.send("ARRI ARRI ARRI ARRI ARRIVEDERCI!")
-
 @client.command(name = "invite")
 async def invite(ctx):
     invite_url = "https://discord.com/api/oauth2/authorize?client_id=813836773758992404&permissions=0&scope=bot"
@@ -33,5 +16,12 @@ async def invite(ctx):
 @client.command(name = "support")
 async def support(ctx):
     await ctx.send("Support Server:\nhttps://discord.gg/EThRPErsQF")
+
+#Import cog
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+        print(f"{filename} has loaded.")
 
 client.run(token)
