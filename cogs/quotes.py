dbf74378778efd4082474e1d763c16691db09c0c
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-25 23:05:46 +0400

    Added a lot more quotes

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -18,7 +18,18 @@ class Quotes(commands.Cog):
             "NIGERUNDAYOO! \n       - Joseph Joestar",
             "There are times when a gentleman has to be courageous and fight, even when his opponent is bigger than he is and he knows he’s going to lose! \n       - Jonathan Joestar",
             "You may have outsmarted me, but I have outsmarted your outsmarting! \n         - Joseph Joestar",
-            "I'LL MAKE YOU CRY LIKE A BABY, DIO!!! \n       - Jonathan Joestar" 
+            "I'LL MAKE YOU CRY LIKE A BABY, DIO!!! \n       - Jonathan Joestar",
+            "SUNLIGHT YELLOW OVERDRIVE! \n      - Jonathan Joestar",
+            "GOODBYE, JOJO! \n      - DIO", 
+            "It was me! Dio! \n         - DIO",
+            "NIIIIIIIIIICEEEEE \n       - Joseph Joestar",
+            "OHHHHHHHH MYYYYY GOOOOOODDDDD \n       - Joseph Joestar",
+            "YES! I AM! \n      - Mohammed Avdol",
+            "Rerorerorerorerorero \n        - Noriyaki Kakyoin",
+            "Nice watch. I'm going to break it so you can never tell time again. Your face, that is. \n         - Jotaro Kujo",
+            "I, Giorno Giovanna, have a dream! \n       - Giorno Giovanna",
+            
+
         ]
         await ctx.send(random.choice(quotes))
 
