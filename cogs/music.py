d9619f3964fdeecda5d499666bf216659f8b4035
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-26 23:00:57 +0400

    Hopefully fixed it

--- cogs/music.py
+++ cogs/music.py
@@ -1,15 +1,16 @@
 import discord
 from discord.ext import commands, tasks
 import youtube_dl
+from discord.voice_client import VoiceClient
 
 class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    youtube_dl.utils.bug_reports_message = lambda: 
 
-    @commands.command(name = "summon", aliases=["connect"])
-    @commands.has_permissions(manage_guild=True)
-    async def _summon(self, ctx):
+    @commands.command(name = "summon", aliases=["play"])
+    async def play(self, ctx):
         if not ctx.message.author.voice:
             await ctx.send("You are not connected to a voice channel!")
             return
