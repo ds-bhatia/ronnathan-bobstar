2590d706fdd4ace19b28261b7b48f25a94da1b51
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-03-02 22:43:55 +0400

    forgot indents

--- cogs/music.py
+++ cogs/music.py
@@ -21,13 +21,13 @@ class Music(commands.Cog):
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
+    }
 
-ffmpeg_options = {
-    'options': '-vn'
-}
+    ffmpeg_options = {
+        'options': '-vn'
+    }
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+    ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
