e3e166a8ed83dc0a02bc40d75b1cbd1d855ec564
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-03-02 22:41:58 +0400

    added some sort of help command(i think)

--- cogs/music.py
+++ cogs/music.py
@@ -8,6 +8,47 @@ class Music(commands.Cog):
         self.client = client
 
     youtube_dl.utils.bug_reports_message = lambda: ''
+    
+    ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
     @commands.command(name = "awaken", aliases=["summon"])
     async def play(self, ctx):
@@ -29,6 +70,14 @@ class Music(commands.Cog):
         else:
             await voice_client.disconnect()
 
-            
+    @commands.command(name = "play")
+    async def play(self, ctx):
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            player  = await YTDLSource.form_url()
+
+
 def setup(client):
     client.add_cog(Music(client))
