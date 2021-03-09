7ff85b42ad45e539e23deebbac9db6dcc7090445
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-03-09 19:39:20 +0400

    updating music.py

--- cogs/music.py
+++ cogs/music.py
@@ -9,46 +9,6 @@ class Music(commands.Cog):
 
     youtube_dl.utils.bug_reports_message = lambda: ''
     
-    ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-    }
-
-    ffmpeg_options = {
-        'options': '-vn'
-    }
-
-    ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
     @commands.command(name = "awaken", aliases=["summon"], brief = "Get the bot to join your voice channel")
     async def play(self, ctx):
@@ -70,14 +30,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
         else:
             await voice_client.disconnect()
 
-    @commands.command(name = "play", brief = "Coming soon")
-    async def play(self, ctx):
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-
-        async with ctx.typing():
-            player  = await YTDLSource.form_url()
-
+    @cat.command(pass_context=True)
+    async def play(self, ctx, url : str):
+        channel = ctx.message.author.voice.channel
 
 def setup(client):
     client.add_cog(Music(client))
