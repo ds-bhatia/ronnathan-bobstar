255afb5437ac10ddc902ea2d0e051166b115aa86
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-03-09 21:41:13 +0400

    Added bot status

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ token = os.environ["TOKEN"]
 
 client = commands.Bot(command_prefix = "j!", help_command=PrettyHelp())
 
+@client.event
+async def on_ready():
+    await client.change_presence(status = discord.Status.online, activity = discord.Game("Watching jojo || j!help"))
 
 #Import cog
 
