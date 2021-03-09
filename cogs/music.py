53e3b94928d1d74f08463a647ad3b127a10d70f9
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-03-09 21:14:28 +0400

    added proper play command

--- cogs/music.py
+++ cogs/music.py
@@ -32,6 +32,33 @@ class Music(commands.Cog):
 
     @commands.command(pass_context=True)
     async def play(self, ctx, url : str):
-        await ctx.send("No u play")
+        song_there = os.path.isfile("song.mp3")
+        try:
+            if song_there:
+                os.remove("song.mp3")
+        except PermissionError:
+            await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+            return
+
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, "song.mp3")
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
 def setup(client):
     client.add_cog(Music(client))
