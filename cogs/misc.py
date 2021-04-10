8e84a777fbe746ac4db1e40935855bc3b3b43683
Scandlex <chrisvarghese23@gmail.com>
2021-04-10 10:52:40 +0400

    yes

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,42 +3,43 @@ import typing
 from typing import Union
 from discord.ext import commands, tasks
 
+
 class Misc(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name = "github",brief = "Go to the github repository")
+    @commands.command(name="github", brief="Go to the github repository")
     async def github(self, ctx):
         github_url = "https://github.com/Technoselbow/ronnathan-bobstar"
         embed = discord.Embed(
-            title = "Click here to go to the Github repo!", url = github_url, color = discord.Color.dark_gray()
+            title="Click here to go to the Github repo!", url=github_url, color=discord.Color.dark_gray()
         )
-        await ctx.send(embed = embed)
+        await ctx.send(embed=embed)
 
-    @commands.command(name = "invite", brief = "Get the invite link to the bot")
+    @commands.command(name="invite", brief="Get the invite link to the bot")
     async def invite(self, ctx):
         invite_url = "https://discord.com/api/oauth2/authorize?client_id=813836773758992404&permissions=0&scope=bot"
         embed = discord.Embed(
-            title = "Click here to invite Ronnathan Bobstar!", url = invite_url, color = discord.Color.dark_gray())
-        await ctx.send(embed = embed)
+            title="Click here to invite Ronnathan Bobstar!", url=invite_url, color=discord.Color.dark_gray())
+        await ctx.send(embed=embed)
 
-    @commands.command(name = "support", brief = "Get the link for the support server")
+    @commands.command(name="support", brief="Get the link for the support server")
     async def support(self, ctx):
         await ctx.send("Support Server:\nhttps://discord.gg/EThRPErsQF")
 
-    @commands.command(name = "reddit", brief = "Get the links for the jojo subreddits")
+    @commands.command(name="reddit", brief="Get the links for the jojo subreddits")
     async def reddit(self, ctx):
         invite_url = "https://www.reddit.com/r/StardustCrusaders/"
         embed = discord.Embed(
-            title = "Click here to go to the jojo subreddit!", url = invite_url, color = discord.Color.dark_gray())
-        await ctx.send(embed = embed)
+            title="Click here to go to the jojo subreddit!", url=invite_url, color=discord.Color.dark_gray())
+        await ctx.send(embed=embed)
         invite_url2 = "https://www.reddit.com/r/ShitpostCrusaders/"
         embed = discord.Embed(
-            title = "Click here to go to the jojo meme subreddit!", url = invite_url2, color = discord.Color.dark_gray())
-        await ctx.send(embed = embed)
+            title="Click here to go to the jojo meme subreddit!", url=invite_url2, color=discord.Color.dark_gray())
+        await ctx.send(embed=embed)
 
-    @commands.command(name = "killerqueen", breif = "Deletes the person's messages",  aliases = ["kq"])
-    @commands.has_guild_permissions(manage_messsages = "True")
+    @commands.command(name="killerqueen", breif="Deletes the person's messages", aliases=["kq"])
+    @commands.has_guild_permissions(manage_messsages = True)
     async def killerqueen(self, ctx, user: Union[discord.Member, int]):
         if user == None:
             await ctx.send("Specify a person smh....")
@@ -49,7 +50,7 @@ class Misc(commands.Cog):
 
         if isinstance(user, int):
             user_str = f"<@{user}>"
-            user = discord.Object(id = user)
+            user = discord.Object(id=user)
 
         else:
             user_str = user
@@ -63,8 +64,6 @@ class Misc(commands.Cog):
             await ctx.delete()
 
         await ctx.send("you have been cursed owo")
-
-
 
 
 def setup(client):
